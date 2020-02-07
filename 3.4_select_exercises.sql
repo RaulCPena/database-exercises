a78f7f762ccda689df53ebf06d7f5500cc2b005b
RaulCPena <raul.c.pena@gmail.com>
2020-02-07 16:45:33 -0600

    updated query console with better syntax

--- 3.4_select_exercises.sql
+++ 3.4_select_exercises.sql
@@ -2,14 +2,27 @@ USE albums_db;
 
 DESCRIBE albums;
 
-SELECT * FROM albums WHERE artist = 'Pink Floyd';
+SELECT NAME AS "Albums by Pink Floyd"
+FROM albums
+WHERE artist = 'Pink Floyd';
 
-SELECT release_date FROM albums WHERE NAME = "Sgt. Pepper's Lonely Hearts Club Band";
+SELECT release_date
+FROM albums
+WHERE NAME = "Sgt. Pepper's Lonely Hearts Club Band";
 
-SELECT genre FROM albums WHERE NAME = 'Nevermind';
+SELECT genre
+FROM albums 
+WHERE NAME = 'Nevermind';
 
-SELECT * FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT NAME AS "Albums released in the 1990's"
+FROM albums
+WHERE release_date
+BETWEEN 1990 AND 1999;
 
-SELECT * FROM albums WHERE sales < 20;
+SELECT * 
+FROM albums
+WHERE sales < 20;
 
-SELECT * FROM albums WHERE genre = 'rock';
+SELECT *
+FROM albums
+WHERE genre = 'rock';
