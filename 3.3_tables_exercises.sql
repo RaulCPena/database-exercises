6bc8fc10504f8018ddd187359db3a899db442880
RaulCPena <raul.c.pena@gmail.com>
2020-02-07 14:52:36 -0600

    created 3.3_tables_exercises.sql query

---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,13 @@
+#1 open sequal pro
+#2 save database name 3.3_tables_exercises.sql
+SHOW DATABASES;
+#4
+USE employees;
+#5
+SHOW TABLES;
+#6 All tables
+#7 All tables
+#8 All tables
+#9 there is no relationship, however this is a relationship between employees and employees_with_departments and between departments and dep_emp
+#10
+SHOW CREATE TABLE dept_manager;
