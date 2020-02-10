47f81c1e1d0efb89204bd67bc2d8a874d018bdb1
RaulCPena <raul.c.pena@gmail.com>
2020-02-10 11:53:11 -0600

    finished second part of where clause exercises

--- 3.5.1_where_exercises.sql
+++ 3.5.1_where_exercises.sql
@@ -2,9 +2,9 @@ SHOW DATABASES;
 
 USE employees;
 
-SELECT first_name
+SELECT first_name, gender
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE (first_name = ('Irena') OR first_name = ('Vidya') OR first_name = ('Maya')) AND gender = 'M';
 
 SELECT last_name
 FROM employees
@@ -21,3 +21,19 @@ WHERE MONTH(birth_date) = 12 AND DAY(birth_date) = 25;
 SELECT last_name
 FROM employees
 WHERE last_name LIKE '%q%';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+SELECT first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE YEAR(hire_date) BETWEEN 1990 AND 2000 AND MONTH(birth_date) = 12 AND DAY(birth_date) = 25;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
