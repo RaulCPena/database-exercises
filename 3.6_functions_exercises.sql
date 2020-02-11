cb7ceae0b23d35f6beadf0ec14ac9339c7964f55
RaulCPena <raul.c.pena@gmail.com>
2020-02-11 13:47:42 -0600

    finished group_by exercies

--- 3.6_functions_exercises.sql
+++ 3.6_functions_exercises.sql
@@ -1,31 +1,59 @@
 USE employees;
 
-SELECT CONCAT(first_name, ' ',last_name) AS full_name
-FROM employees
-WHERE first_name LIKE 'e%' AND last_name LIKE '%e';
+-- Exercise 2
 
-SELECT UPPER(CONCAT(first_name, ' ',last_name)) AS full_name
-FROM employees
-WHERE first_name LIKE 'e%' AND last_name LIKE '%e';
+SELECT DISTINCT title
+FROM titles;
 
-SELECT DATEDIFF(NOW(), hire_date)
-FROM employees
-WHERE hire_date LIKE '199%'
-AND birth_date LIKE '%-12-25';
+-- Exercise 3
 
-SELECT 
-    MIN(salary),
-    MAX(salary)
-FROM salaries;
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY last_name;
+
+-- Exercise 4
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY last_name;
+
+-- Exercise 5
+
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+-- Exercise 6
+
+SELECT gender, COUNT(*)
+FROM employees 
+WHERE (first_name = 'Irena' OR 
+first_name = 'Vidya' OR 
+first_name = 'Maya')
+GROUP BY gender;
+
+-- Exercise 7
+
+
+SELECT COUNT(*), gender
+FROM employees 
+WHERE (first_name = 'Irena' OR 
+first_name = 'Vidya' OR 
+first_name = 'Maya')
+GROUP BY gender;
+
+-- Exercise Bonus
 
 SELECT (LOWER(CONCAT(SUBSTR(first_name,1, 1), SUBSTR(last_name, 1, 4), '_', 
     SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2))))
-    AS user_name
+    AS user_name, COUNT(*)
 FROM employees
-LIMIT 10;
+GROUP BY user_name
+HAVING COUNT(user_name) > 1;
 
-SELECT first_name, last_name,
-YEAR(NOW()) - YEAR(hire_date) 
-AS Years_with_Company
-FROM employees
-LIMIT 50;
+-- BONUS answer
+
+# There are 13,251 unique user_name'so
