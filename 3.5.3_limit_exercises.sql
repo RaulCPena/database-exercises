6c97e94d5b6321b4540d5a9cb1c9f0bce49fbd01
RaulCPena <raul.c.pena@gmail.com>
2020-02-11 09:04:54 -0600

    rewrote querys as more efficient ones

--- 3.5.3_limit_exercises.sql
+++ 3.5.3_limit_exercises.sql
@@ -10,7 +10,13 @@ SELECT DISTINCT title FROM titles;
 
 SELECT DISTINCT last_name
 FROM employees
-ORDER BY last_name DESC LIMIT 10;
+ORDER BY last_name DESC 
+LIMIT 10;
+
+SELECT DISTINCT last_name
+FROM employees
+GROUP BY last_name DESC
+LIMIT 10;
 
 -- Exercise 3
 
@@ -27,8 +33,14 @@ SELECT *
 FROM employees
 WHERE hire_date LIKE '199%' 
 AND birth_date LIKE '%-12-25'
-ORDER BY birth_date, hire_date DESC
+ORDER BY birth_date ASC, hire_date DESC
 LIMIT 5 OFFSET 45;
+
+
+
+/* Relationship BETWEEN LIMIT AND OFFSET
+
+(page -1) * LIMIT = offset */
 
 
 
